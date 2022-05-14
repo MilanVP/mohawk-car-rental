d5ada5e14a563776c520f74080358250f60b8553
Milan Prajapati <milanprajapati440@mal.com>
2022-05-13 20:09:57 -0400

    First Commit

---
+++ assignment1/Models/PurchaseDateValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment1.Models
+{
+    public class PurchaseDateValidation : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var purchaseDate = Convert.ToDateTime(value);
+            return purchaseDate <= DateTime.Now;
+        }
+    }
+}
